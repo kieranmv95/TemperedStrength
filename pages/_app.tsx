8f0e802fbf4a8c42f86917f2a9b95ea5a4f5ad18
kieranvenison <venison@hey.com>
2023-05-16 13:35:38 +0100

    feat: ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,33 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { Open_Sans } from "next/font/google";
-import "@/styles/globals.css";
 import { Header, Footer } from "@/components";
+
+import "@/styles/globals.css";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={font.className}>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KBJ19TPWHW"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-KBJ19TPWHW');
+      `}
+      </Script>
+      <div className={font.className}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
   );
 }
